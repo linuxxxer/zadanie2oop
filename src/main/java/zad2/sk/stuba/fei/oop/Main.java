672004e5d7ad22609fc13f67e550401182756a6d
linuxxxer <balint.ruzinsky@gmail.com>
2019-04-10 16:14:12 +0200

    Initial commit - project_start removed some things, added some things...

---
+++ src/main/java/zad2/sk/stuba/fei/oop/Main.java
@@ -0,0 +1,27 @@
+package sk.stuba.fei.oop;
+
+import sk.stuba.fei.oop.generated.Document;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            InputStream resource = ClassLoader.getSystemResourceAsStream("insurance.xml");
+            JAXBContext context = JAXBContext.newInstance(Document.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+
+            Document document = (Document) unmarshaller.unmarshal(resource);
+
+            System.out.println("Number of places:" + document.getPlace().size());
+            System.out.println("Number of tranistions:" + document.getTransition().size());
+            System.out.println("Number of arcs:" + document.getArc().size());
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/zad2/sk/stuba/fei/oop/generated/Transition.java
@@ -0,0 +1,121 @@
+
+package sk.stuba.fei.oop.generated;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element ref="{}id"/&gt;
+ *         &lt;element ref="{}x"/&gt;
+ *         &lt;element ref="{}y"/&gt;
+ *         &lt;element ref="{}label"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "id",
+    "x",
+    "y",
+    "label"
+})
+@XmlRootElement(name = "transition")
+public class Transition {
+
+    protected int id;
+    protected int x;
+    protected int y;
+    @XmlElement(required = true)
+    protected String label;
+
+    /**
+     * Gets the value of the id property.
+     * 
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets the value of the id property.
+     * 
+     */
+    public void setId(int value) {
+        this.id = value;
+    }
+
+    /**
+     * Gets the value of the x property.
+     * 
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Sets the value of the x property.
+     * 
+     */
+    public void setX(int value) {
+        this.x = value;
+    }
+
+    /**
+     * Gets the value of the y property.
+     * 
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Sets the value of the y property.
+     * 
+     */
+    public void setY(int value) {
+        this.y = value;
+    }
+
+    /**
+     * Gets the value of the label property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getLabel() {
+        return label;
+    }
+
+    /**
+     * Sets the value of the label property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setLabel(String value) {
+        this.label = value;
+    }
+
+}
