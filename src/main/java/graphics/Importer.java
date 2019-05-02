05376b86cb9f4e37ea7522a32a321821394c5026
linuxxxer <balint.ruzinsky@gmail.com>
2019-05-02 11:11:06 +0200

    Release ready

--- src/main/java/graphics/Importer.java
+++ src/main/java/graphics/Importer.java
@@ -5,19 +5,24 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 public class Importer {
 
     public Document importing(String pathToFile){
         try {
-            File file = new File(pathToFile);
-            InputStream resource = ClassLoader.getSystemResourceAsStream(file.getName());
-            JAXBContext context = JAXBContext.newInstance(Document.class);
-            Unmarshaller unmarshaller = context.createUnmarshaller();
+            File file                   =  new File(pathToFile);
+
+            InputStream resource        =  new FileInputStream(file.getAbsolutePath());
+            JAXBContext context         =  JAXBContext.newInstance(Document.class);
+            Unmarshaller unmarshaller   =  context.createUnmarshaller();
 
             return (Document) unmarshaller.unmarshal(resource);
         } catch ( JAXBException e){
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         return null;
